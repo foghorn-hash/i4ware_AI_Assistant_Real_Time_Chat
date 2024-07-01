3c0e4edb680f4f655b4dc71db68d32424a53bde0
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2024-07-01 10:06:34 +0300

    Initial Commit.

--- saas-app/app/Models/Message.php
+++ saas-app/app/Models/Message.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
