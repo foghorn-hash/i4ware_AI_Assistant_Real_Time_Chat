06eee1888202f719c7b0daab05bc3cf2929258ee
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2026-04-16 15:56:44 +0300

    Initial 2 migration files was missing. Added them.

---
+++ saas-app/database/migrations/2025_11_24_054805_add_system_var_to_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->boolean('system_var')
+                ->default(false)
+                ->after('setting_value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('system_var');
+        });
+    }
+};
+
